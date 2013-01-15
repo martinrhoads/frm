2974582defdf4eecc5184acd1f135793f3fbefee
Martin Rhoads <martin.rhoads@airbnb.com>
2013-01-14 17:47:58 -0800

    bug with prefix and aws access key name

--- lib/frm/s3.rb
+++ lib/frm/s3.rb
@@ -5,32 +5,31 @@ module FRM
 
     def initialize(opts={})
       super()
-      @opts = opts
+      @opts = opts.dup
+      puts "@opts.inspect is #{@opts.inspect}"
       @opts[:public_repo] ||= false
       @opts[:acl] = @opts[:public_repo] ? :public_read : :private
-      @opts[:aws_access_key] ||= ENV['AWS_ACCESS_KEY_ID']
-      @opts[:aws_secret_key] ||= ENV['AWS_SECRET_ACCESS_KEY']
-      raise "you either need to pass an aws_access_key option or set the AWS_ACCESS_KEY environment variable" \
-        if @opts[:aws_access_key].nil?
-      raise "you either need to pass an aws_secret_key option or set the AWS_SECRET_KEY environment variable" \
-        if @opts[:aws_secret_key].nil?
+      @opts[:access_key_id] ||= ENV['AWS_ACCESS_KEY_ID']
+      @opts[:secret_access_key] ||= ENV['AWS_SECRET_ACCESS_KEY']
+      raise "you either need to pass an aws_access_key option or set the AWS_ACCESS_KEY environment variable" if @opts[:access_key_id].nil?
+      raise "you either need to pass an aws_secret_key option or set the AWS_SECRET_KEY environment variable" if @opts[:secret_access_key].nil?
       raise "you need to pass in a bucket param" unless @opts[:bucket]
       raise "you need to pass in a prefix param" unless @opts[:prefix]
-      @opts[:prefix] << '/' unless @opts[:prefix][-1] == '/'
-      AWS.config(:access_key_id => @opts[:access_key_id], 
+      @opts[:prefix] = @opts[:prefix] +  '/' unless @opts[:prefix][-1] == '/'
+      AWS.config(:access_key_id => @opts[:access_key_id],
                  :secret_access_key => @opts[:secret_access_key])
       @s3 = AWS::S3.new
     end
     
     def exists?(relative_path)
-      handle_errors do 
+      handle_errors do
         return @s3.buckets[@opts[:bucket]].objects[full_path(relative_path)].exists?
       end
     end
 
     def etag(relative_path)
       quoted_etag = ""
-      handle_errors do 
+      handle_errors do
         quoted_etag = @s3.buckets[@opts[:bucket]].objects[full_path(relative_path)].etag
       end
       # stupid method call is actually putting quotes in the string,
@@ -39,20 +38,20 @@ module FRM
     end
 
     def put(relative_path,value)
-      handle_errors do 
-        @s3.buckets[@opts[:bucket]].objects[full_path(relative_path)].write(value,acl: @opts[:acl]) # 
+      handle_errors do
+        @s3.buckets[@opts[:bucket]].objects[full_path(relative_path)].write(value,acl: @opts[:acl]) #
       end
       return true
     end
-    
+
     def get(relative_path)
-      handle_errors do 
+      handle_errors do
         return @s3.buckets[@opts[:bucket]].objects[full_path(relative_path)].read
       end
     end
-    
+
     def delete(relative_path)
-      handle_errors do 
+      handle_errors do
         @s3.buckets[@opts[:bucket]].objects[full_path(relative_path)].delete
       end
       return true
@@ -73,3 +72,4 @@ module FRM
 
   end
 end
+
